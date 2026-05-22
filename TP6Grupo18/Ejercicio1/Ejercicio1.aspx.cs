3881b033aababecededd165ecdfd0f4f6bd98ffa
SantiB <151892199+SanB00@users.noreply.github.com>
2026-05-21 22:45:06 -0300

    Estructura de webForms

---
+++ TP6Grupo18/Ejercicio1/Ejercicio1.aspx.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TP6Grupo18
+{
+    public partial class Ejercicio1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
