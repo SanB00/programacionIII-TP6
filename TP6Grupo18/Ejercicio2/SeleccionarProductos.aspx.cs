47dc1ad5493bbf6f77f0406e116d8de7d47c5cf9
SantiB <151892199+SanB00@users.noreply.github.com>
2026-05-26 16:22:18 -0300

    de Elian Gaston Maspero - martes, 26 de mayo de 2026, 15:09 Buenas , continuando con el segundo ejercicio, procedi a desarrollar la funcion CrearTabla y AgregarFila, para incluirlas en en el sistema
    Adjunto imagen_2026-05-26_150853376.png
    Adjunto imagen_2026-05-26_150922383.png
     TP6Grupo18-v2.4.rar

--- TP6Grupo18/Ejercicio2/SeleccionarProductos.aspx.cs
+++ TP6Grupo18/Ejercicio2/SeleccionarProductos.aspx.cs
@@ -26,11 +26,50 @@ namespace TP6Grupo18.Ejercicio2
         protected void gvSeleccionar_SelectedIndexChanged(object sender, EventArgs e) {
             string idProducto = ((Label)gvSeleccionar.SelectedRow.FindControl("lbl_it_idProdu")).Text;
             string nombreProducto = ((Label)gvSeleccionar.SelectedRow.FindControl("lb_it_NombreProd")).Text;
+            string idProveedor = ((Label)gvSeleccionar.SelectedRow.FindControl("lb_it_Prov")).Text;
+            string precioUnidad = ((Label)gvSeleccionar.SelectedRow.FindControl("lb_it_PrecioU")).Text;
+
+            if (Session["tabla"] == null) {
+                Session["tabla"] = crearTabla();
+            }
+            
+            DataTable tablaSession = (DataTable)Session["tabla"];
+
+            agregarFila(tablaSession, idProducto, nombreProducto, idProveedor, precioUnidad);
 
             lblSelect.Text = "Artículo agregado: " + nombreProducto;
-
-
         }
+        private DataTable crearTabla() {
+            DataTable dataTable = new DataTable();
+
+            DataColumn dataColumn = new DataColumn("IdProducto", System.Type.GetType("System.String"));
+            dataTable.Columns.Add(dataColumn);
+
+            dataColumn = new DataColumn("NombreProducto", System.Type.GetType("System.String"));
+            dataTable.Columns.Add(dataColumn);
+
+            dataColumn = new DataColumn("IdProveedor", System.Type.GetType("System.String"));
+            dataTable.Columns.Add(dataColumn);
+
+            dataColumn = new DataColumn("PrecioUnidad", System.Type.GetType("System.String"));
+            dataTable.Columns.Add(dataColumn);
+
+            return dataTable;
+        }
+
+        private DataTable agregarFila(DataTable dataTable, string idProducto, string nombreProducto, string idProveedor, string precioUnidad) {
+            DataRow dataRow = dataTable.NewRow();
+
+            dataRow["IdProducto"] = idProducto;
+            dataRow["NombreProducto"] = nombreProducto;
+            dataRow["IdProveedor"] = idProveedor;
+            dataRow["PrecioUnidad"] = precioUnidad;
+
+            dataTable.Rows.Add(dataRow);
+
+            return dataTable;
+        }
+
 
     }
 
